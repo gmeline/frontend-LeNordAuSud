b256b51448383262c130767b4064cdeae3bf41b0
Méline <gmeline@macbook-pro-vr.home>
2024-12-23 21:13:06 +0100

    feat(front:localisation):MEP page de la localisation et évènement

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Accueil from "./components/Accueil";
 import BarreNavigation from "./components/barreNavigation";
 import Carte from "./components/Carte";
+import Localisation from "./components/Localisation";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Accueil />} />
           <Route path="/carte" element={<Carte />} />
+          <Route path="/localisation" element={<Localisation />} />
         </Routes>
       </div>
     </Router>
