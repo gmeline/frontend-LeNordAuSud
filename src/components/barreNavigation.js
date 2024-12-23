3da49f8e62e9015423f2bb99348109b1c6e1a8b3
Méline <gmeline@macbook-pro-vr.home>
2024-12-23 22:26:29 +0100

    feat(front:barrenavigation):MEP logo et fond

--- src/components/barreNavigation.js
+++ src/components/barreNavigation.js
@@ -1,31 +1,44 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./BarreNavigation.css"; 
+import "./BarreNavigation.css";
+import logo from "../assets/logo.png";
+
 
 function BarreNavigation() {
-  const [isOpen, setIsOpen] = useState(false);
+ const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
-  return (
-    <div className="barre-navigation">
-      <button className="burger" onClick={toggleMenu}>
-        <div className={`line ${isOpen ? "rotate1" : ""}`}></div>
-        <div className={`line ${isOpen ? "hide" : ""}`}></div>
-        <div className={`line ${isOpen ? "rotate2" : ""}`}></div>
-      </button>
+ const toggleMenu = () => {
+   setIsOpen(!isOpen);
+ };
 
-      <nav className={`nav ${isOpen ? "open" : ""}`}>
-        <ul>
-          <li><Link to="/">Accueil</Link></li>
-          <li><Link to="/carte">Carte</Link></li>
-          <li><Link to="/localisation">Localisation</Link></li>
-        </ul>
-      </nav>
-    </div>
-  );
+
+ // Ferme le menu lorsqu'un lien est cliqué
+ const handleLinkClick = () => {
+   setIsOpen(false);
+ };
+
+
+ return (
+   <div className="barre-navigation">
+     <img src={logo} alt="Le Nord au Sud" className="logo" />
+     <button className="burger" onClick={toggleMenu}>
+       <div className={`line ${isOpen ? "rotate1" : ""}`}></div>
+       <div className={`line ${isOpen ? "hide" : ""}`}></div>
+       <div className={`line ${isOpen ? "rotate2" : ""}`}></div>
+     </button>
+
+
+     <nav className={`nav ${isOpen ? "open" : ""}`}>
+       <ul>
+         <li><Link to="/" onClick={handleLinkClick}>Accueil</Link></li>
+         <li><Link to="/carte" onClick={handleLinkClick}>Carte</Link></li>
+         <li><Link to="/localisation" onClick={handleLinkClick}>Localisation</Link></li>
+       </ul>
+     </nav>
+   </div>
+ );
 }
+
 
 export default BarreNavigation;
