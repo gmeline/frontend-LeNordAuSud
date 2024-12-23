0d1386d310d3a2bd86afea7fb840b4165aa5c816
Méline <gmeline@macbook-pro-vr.home>
2024-12-23 20:30:54 +0100

    feat(front:accueil):MEP page d'accueil

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,30 +1,18 @@
 import React from "react";
-import carte from "../assets/carte.jpg";
 import './Accueil.css';
+import photo_camion from "../assets/photo_camion.jpg";
 
 function Accueil() {
   return (
     <div className="container">
-      <h1 className="title">Bienvenue chez Le Nord au Sud</h1>
-      <h3 className="title2">Notre carte :</h3>
-      <img
-        src={carte}
-        alt="Le Nord au Sud"
-        className="image"
-      />
-      <p className="text">
-        Découvrez les meilleures frites artisanales du food truck "Le Nord au Sud". Nous apportons
-        une expérience unique, mêlant saveurs du Nord et ambiance chaleureuse.
-      </p>
-      <h3 className="title2">Localisation :</h3>
-      <ul className="list">
-        <li>Mardi soir : ???</li>
-        <li>Mercredi soir : Castillanes de Laroque d'Olmes</li>
-        <li>Jeudi soir : Bastide sur l'Hers</li>
-        <li>Vendredi midi et soir : Castillanes de Laroque D'Olmes</li>
-        <li>Samedi soir : ???</li>
-        <li>Dimanche soir : Parking du Castel d'Olmes</li>
-      </ul>
+      <div className="image-container">
+        <img src={photo_camion} alt="Le Nord au Sud" className="image1" />
+        <h1 className="title">Bienvenue chez Le Nord au Sud</h1>
+      </div>
+      <p className="text1">Prouvez ici la convivialité des gens du nord, accompagnant à merveille la délicieuse <span className="style1">frite fraîche cuite au gras de bœuf</span>.</p>
+      <p className="text2">Laissez-vous faire découvrir les légendaires <span className="style2">fricadelles, mexicanos, poulycroc,</span> seul ou en <u>"Américain"</u>.</p>
+      <p className="text3">Savourez chez nous de délicieuses <span className="style2">brochettes marinées "maison", nos sauces "maison"</span> et chaque semaine une recette différente des spécialités du nord, concoctée par nos soins.</p>
+      <p className="text4">Notre pain, fabriqué par notre boulanger local, est certifié par le label <span className="style2">"NOU"</span>, issu d'un collectif 100% ariégeois.</p>
     </div>
   );
 }
